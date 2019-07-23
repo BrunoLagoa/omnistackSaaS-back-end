158cf7299f262c1c6adcf22cb4e09ced98e8a1a0
Bruno Lagoa <miroldols@gmail.com>
2019-07-23 02:07:46 -0300

    Changes to be committed: 	modified:   app/Models/Team.js 	modified:   database/seeds/DatabaseSeeder.js 	modified:   package-lock.json 	modified:   package.json 	modified:   start/app.js

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -10,11 +10,20 @@
 |
 */
 
-/** @type {import('@adonisjs/lucid/src/Factory')} */
-const Factory = use('Factory')
+const User = use('App/Models/User')
 
 class DatabaseSeeder {
   async run () {
+    const user = await User.create({
+      name: 'Bruno Castro',
+      email: 'miroldols@gmail.com',
+      password: '123456'
+    })
+
+    await user.teams().create({
+      name: 'Rocketseat',
+      user_id: user.id
+    })
   }
 }
 
