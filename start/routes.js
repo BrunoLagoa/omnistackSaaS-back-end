27e828f17a63082f12ec4564ceb4759789e32051
Bruno Lagoa <miroldols@gmail.com>
2019-08-02 12:03:50 -0300

    - CRUD Projetos Changes to be committed: 	modified:   app/Controllers/Http/ProjectController.js 	modified:   app/Models/Team.js 	modified:   start/routes.js

--- start/routes.js
+++ start/routes.js
@@ -15,4 +15,5 @@ Route.group(() => {
 
 Route.group(() => {
   Route.post('invites', 'InviteController.store')
+  Route.resource('projects', 'ProjectController').apiOnly()
 }).middleware(['auth', 'team'])
